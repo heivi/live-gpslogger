78b83544a054ee0ff5f42a748337628e84e19331
Heikki Virekunnas <heikki@virekunnas.fi>
2015-10-07 19:20:04 +0300

    WP8-versio päivitys, putsattavaa

---
+++ cordova/platforms/wp8/www/cordova_plugins.js
@@ -0,0 +1,28 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/de.appplant.cordova.plugin.background-mode/www/background-mode.js",
+        "id": "de.appplant.cordova.plugin.background-mode.BackgroundMode",
+        "clobbers": [
+            "cordova.plugins.backgroundMode",
+            "plugin.backgroundMode"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-geolocation": "1.0.1",
+    "cordova-plugin-whitelist": "1.0.0",
+    "de.appplant.cordova.plugin.background-mode": "0.6.4",
+    "org.apache.cordova.device": "0.3.0"
+}
+// BOTTOM OF METADATA
+});
